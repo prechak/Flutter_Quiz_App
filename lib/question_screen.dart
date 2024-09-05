5773f96c352a0f0214b254e234d8a4801822708f
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2024-09-06 02:07:22 +0700

    feat: configure column

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -12,6 +12,33 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Center(child: Text('QuestionScreen'));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          )
+        ],
+      ),
+    );
   }
 }
